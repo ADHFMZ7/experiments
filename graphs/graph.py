5afe8ba6d845987d19fa51b24322e947739be97b
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-09-08 23:48:54 -0700

    formatting

--- graphs/graph.py
+++ graphs/graph.py
@@ -9,23 +9,24 @@ class graph:
     self.V = 0
     self.E = 0
 
+
   def find_node(self, label):
     for i in self.vertices.keys():
       if i.label == label:
         return i
     return False
 
+
   def new_node(self, label, val=0):
     self.vertices[node(label, val, self.V)] = []
     self.V += 1
+
 
   def new_edge(self, label1, label2):
     self.E += 1
     self.vertices[self.find_node(label1)].append(label2) 
     self.edges.append(f'{label1}{label2}')
-    
-   
-# g['A'] = 'B'   
+     
     
   def __setitem__(self, label1, label2):
     if not self.find_node(label1):
@@ -39,7 +40,8 @@ class graph:
     if not self.find_node(label):
       self.new_node(label)
     return self.find_node(label)
-
+ 
+  
   def __delitem__(self, label):
     self.V -= 1
     for i, e in enumerate(self.edges):
@@ -50,11 +52,12 @@ class graph:
     for i in list(self.vertices):
       if label in self.vertices[i]:
         self.vertices[i].remove(label)
-    
     del self.vertices[self.find_node(label)]
+  
    
   def __repr__(self):
     return repr(self.vertices).replace(', n', ',\nn').replace('{', '{\n').replace('}', '\n}')
+   
     
   def draw(self):
     dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
@@ -63,6 +66,7 @@ class graph:
     for i in self.edges:
       dot.edge(i[0], i[1])
     return dot
+
 
 @dataclass(frozen=True)
 class node:
